b3c385e979f4a4940d48439ad09bf0e2aa97991f
Chaojun Tan <495825947@qq.com>
2021-06-13 18:57:55 +0800

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,8 +30,14 @@ aprs_gateway="euro.aprs2.net"
 aprs_gateway_port=14580
 status_text="RTL-SDR on $(uname -m) $(uname -s) using PyMultimonAPRS iGate"
 
+# any, ipv4, ipv6
+default_preferred_protocol="any"
+
+default_append_callsign="true"
+default_source="rtl"
 default_ppm="0"
 default_device_index="0"
+default_device="default"
 default_latitude="0.000000"
 default_longitude="0.000000"
 default_table="R"
@@ -61,6 +67,18 @@ if [ -z "$GATEWAY" ]; then
 GATEWAY="${aprs_gateway}:${aprs_gateway_port}"
 fi
 
+if [ -z "$PREFERRED_PROTOCOL" ]; then
+PREFERRED_PROTOCOL="$default_preferred_protocol"
+fi
+
+if [ -z "$APPEND_CALLSIGN" ]; then
+APPEND_CALLSIGN="$default_append_callsign"
+fi
+
+if [ -z "$SOURCE" ]; then
+SOURCE="$default_source"
+fi
+
 if [ -z "$FREQ" ]; then
 FREQ="$frequency"
 fi
@@ -75,6 +93,10 @@ fi
 
 if [ -z "$DEVICE_INDEX" ]; then
 DEVICE_INDEX="$default_device_index"
+fi
+
+if [ -z "$DEVICE" ]; then
+DEVICE="$default_device"
 fi
 
 if [ -z "$LAT" ]; then
@@ -119,7 +141,10 @@ cp "$CONFIG_FILE" "$tmp_dir/pymultimonaprs.json" && CONFIG_FILE="$tmp_dir/pymult
 
 sed -i "s/\"callsign\": [^,]*,/\"callsign\": \"$CALLSIGN\",/g" $CONFIG_FILE
 sed -i "s/\"passcode\": [^,]*,/\"passcode\": \"$APRSKEY\",/g" $CONFIG_FILE
-sed -i "s/\"gateway\": [^,]*,/\"gateway\": \[\"$GATEWAY\",/g" $CONFIG_FILE
+
+#sed -i "s/\"gateway\": [^,]*,/\"gateway\": \[\"$GATEWAY\",/g" $CONFIG_FILE
+sed -i "s/\"gateway\": [^]]*\],/\"gateway\": \[\"$GATEWAY\"\],/g" $CONFIG_FILE
+
 sed -i "s/\"freq\": [^,]*,/\"freq\": $FREQ,/g" $CONFIG_FILE
 sed -i "s/\"ppm\": [^,]*,/\"ppm\": $PPM,/g" $CONFIG_FILE
 sed -i "s/\"gain\": [^,]*,/\"gain\": $GAIN,/g" $CONFIG_FILE
@@ -127,7 +152,15 @@ sed -i "s/\"lat\": [^,]*,/\"lat\": $LAT,/g" $CONFIG_FILE
 sed -i "s/\"lng\": [^,]*,/\"lng\": $LNG,/g" $CONFIG_FILE
 sed -i "s|\"text\": [^,]*,|\"text\": \"$TEXT\",|g" $CONFIG_FILE
 
+
+sed -i "s/\"preferred_protocol\": [^,]*,/\"preferred_protocol\": \"$PREFERRED_PROTOCOL\",|g" $CONFIG_FILE
+sed -i "s/\"append_callsign\": [^,]*,/\"append_callsign\": $APPEND_CALLSIGN,/g" $CONFIG_FILE
+sed -i "s/\"source\": [^,]*,/\"source\": \"$SOURCE\",|g" $CONFIG_FILE
+
+[ ! -z "$OFFSET_TUNING" ] && sed -i "s/\"offset_tuning\": [^,]*,/\"offset_tuning\": $OFFSET_TUNING,/g" $CONFIG_FILE
+
 sed -i "s/\"device_index\": [^,]*,/\"device_index\": $DEVICE_INDEX,/g" $CONFIG_FILE
+sed -i "s|\"device\": [^,]*,|\"device\": \"$DEVICE\",|g" $CONFIG_FILE
 sed -i "s|\"table\": [^,]*,|\"table\": \"$TABLE\",|g" $CONFIG_FILE
 sed -i "s|\"symbol\": [^,]*,|\"symbol\": \"$SYMBOL\",|g" $CONFIG_FILE
 sed -i "s|\"comment\": [^,]*,|\"comment\": \"$COMMENT\",|g" $CONFIG_FILE
